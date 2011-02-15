703587b0204032c251304af479e182a879fe28f2
James Rowe <jnrowe@gmail.com>
2011-02-15 14:12:48 +0000

    Added script header.
    Copyright junk, docstring, etc.

--- versionah.py
+++ versionah.py
@@ -1,4 +1,44 @@
 #! /usr/bin/python -tt
+"""versionah - Simple version specification management"""
+# Copyright (C) 2011  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__version__ = "0.1.0"
+__date__ = "2011-02-15"
+__author__ = "James Rowe <jnrowe@gmail.com>"
+__copyright__ = "Copyright (C) 2011  James Rowe <jnrowe@gmail.com>"
+__license__ = "GNU General Public License Version 3"
+__credits__ = ""
+__history__ = "See git repository"
+
+from email.utils import parseaddr
+
+__doc__ += """.
+
+versionah is a GPL v3 licensed module for maintaining version information files
+for use in project management.
+
+.. moduleauthor:: `%s <mailto:%s>`__
+""" % parseaddr(__author__)
+
+# Pull the first paragraph from the docstring
+USAGE = __doc__[:__doc__.find('\n\n', 100)].splitlines()[2:]
+# Replace script name with optparse's substitution var, and rebuild string
+USAGE = "\n".join(USAGE).replace("versionah", "%prog")
+
 
 def bump(version, bump_type):
     """Bump a version string
