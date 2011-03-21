5533dea3549892b7dd59e68d5b46064d41ffe0c1
James Rowe <jnrowe@gmail.com>
2011-03-21 11:10:20 +0000

    Added MANIFEST.in to include package data in sdist output.

--- versionah/_version.py
+++ versionah/_version.py
@@ -1,5 +1,5 @@
-# This is versionah version 0.9.0 (2011-03-21)
-dotted = "0.9.0"
-libtool = "9:20"
-hex = 0x000900
+# This is versionah version 0.9.1 (2011-03-21)
+dotted = "0.9.1"
+libtool = "9:21"
+hex = 0x000901
 date = "2011-03-21"
