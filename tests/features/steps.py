4f8d8d5d6c9958b947630d227046f9a5e50bd210
James Rowe <jnrowe@gmail.com>
2011-03-31 14:14:29 +0000

    Small PEP 8 fixes to lettuce steps.

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -21,49 +21,61 @@ def step(match):
                        if s.startswith("VALID_")])
     return lettuce_step(match % valid_dict)
 
+
 @step(u'I have the version (%(VERSION)s)')
 def have_the_version(step, version):
     world.version = versionah.Version(versionah.split_version(version))
+
 
 @step(u'I bump its major version')
 def bump_major_version(step):
     world.version.bump("major")
 
+
 @step(u'I bump its minor version')
 def bump_minor_version(step):
     world.version.bump("minor")
+
 
 @step(u'I bump its micro version')
 def bump_micro_version(step):
     world.version.bump("micro")
 
+
 @step(u'I see the version (%(VERSION)s)')
 def see_the_version(step, expected):
     assert_equal(world.version.as_dotted(), expected)
+
 
 @step(u'I display its dotted representation')
 def display_dotted_representation(step):
     world.string = world.version.as_dotted()
 
+
 @step(u'I display its hex representation')
 def display_hex_representation(step):
     world.string = world.version.as_hex()
+
 
 @step(u'I display its libtool representation')
 def display_libtool_representation(step):
     world.string = world.version.as_libtool()
 
+
 @step(u'I see the string (.*)')
 def see_the_string(step, expected):
     assert_equal(world.string, expected)
+
 
 @step(u'I have the file (.*)')
 def have_the_file(step, name):
     world.name = name
 
+
 @step(u'I read its content')
 def read_content(step):
     world.version = versionah.Version.read("tests/data/%s" % world.name)
+
 
 @step(u'I write its value to (.*)')
 def write_value_to_file(step, name):
@@ -73,26 +85,32 @@ def write_value_to_file(step, name):
     step.given("I read its content")
     os.unlink("tests/data/%s" % name)
 
+
 @step(u'I have the package (%(PACKAGE)s) version (%(VERSION)s)')
 def have_named_version(step, name, version):
     world.version = versionah.Version(versionah.split_version(version), name)
+
 
 @step(u'I display its string representation')
 def display_string_representation(step):
     world.string = str(world.version)
 
+
 @step(u"I find today's date")
 def find_todays_date(step):
     assert_equal(world.version.date, datetime.date.today())
+
 
 @step(u'I bump its patch version')
 def bump_patch_version(step):
     world.version.bump("patch")
 
+
 @step(u'I have the versions (%(VERSION)s) and (%(VERSION)s)')
 def have_versions(step, version1, version2):
     world.version1 = versionah.Version(versionah.split_version(version1))
     world.version2 = versionah.Version(versionah.split_version(version2))
+
 
 @step(u'I compare them for equality')
 def compare_equality(step):
@@ -101,13 +119,16 @@ def compare_equality(step):
     except Exception as e:
         world.exception = e
 
+
 @step(u'I see the comparison (.*)')
 def see_comparison_result(step, expected):
     assert_equal(world.result, expected)
 
+
 @step(u'I search for the greatest')
 def find_max(step):
     world.version = max((world.version1, world.version2))
+
 
 @step(u'I have the invalid version (.*)')
 def have_invalid_version(step, string):
@@ -116,34 +137,44 @@ def have_invalid_version(step, string):
     except Exception as e:
         world.exception = e
 
+
 @step(u'I receive (.*)')
 def receive_exception(step, expected):
     assert_equal(unicode(world.exception.__class__.__name__), expected)
+
 
 @step(u'I pass it to (.*)')
 def pass_to_function(step, function):
     world.result = getattr(versionah, function)([world.name, ])
 
+
 @step(u'I pass (.*) argument ([^ ]+) to (.*)')
 def pass_args_to_function(step, arg, value, function):
-    world.result = getattr(versionah, function)(["--%s" % arg, value, world.name])
+    world.result = getattr(versionah, function)(["--%s" % arg, value,
+                                                 world.name])
+
 
 @step(u'I see the file type (.*)')
 def see_file_type(step, file_type):
     assert_equal(world.result[0].file_type, file_type)
 
+
 @step(u'I process (.*) with (.*)')
 def process_with_linter(step, name, linter):
     file_type = versionah.process_command_line([name, ])[0].file_type
     world.version.write("tests/data/%s" % name, file_type)
-    world.retval = subprocess.call(linter.split() + ["tests/data/%s" %name, ],
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
+    world.retval = subprocess.call(
+        linter.split() + ["tests/data/%s" % name, ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
     os.unlink("tests/data/%s" % name)
+
 
 @step(u'linter returns 0')
 def checker_returns_zero(step):
     assert_equal(world.retval, 0)
+
 
 @step(u'I have the version (%(VERSION)s) created on (%(DATE)s)')
 def have_the_version_with_date(step, version, date):
@@ -151,25 +182,32 @@ def have_the_version_with_date(step, version, date):
     world.version = versionah.Version(versionah.split_version(version),
                                       date=datetime.date(y, m, d))
 
+
 @step(u'I display its date representation')
 def display_date_representation(step):
     world.string = world.version.as_date()
+
 
 @step(u'I see the date string (.*)')
 def see_the_date(step, expected):
     assert_equal(world.string, expected)
 
-@step(u'I have the Version object for (%(VERSION)s) and (?:tuple|string) (%(VERSION)s)')
+
+@step(u'I have the Version object for (%(VERSION)s) and (?:tuple|string) ' \
+      '(%(VERSION)s)')
 def have_version_object_and_tuple(step, version1, version2):
     world.version1 = versionah.Version(versionah.split_version(version1))
     world.version2 = versionah.split_version(version2)
+
 
 @step(u'I have the Version object for (%(VERSION)s) and list (%(VERSION)s)')
 def have_version_object_and_list(step, version1, version2):
     world.version1 = versionah.Version(versionah.split_version(version1))
     world.version2 = list(versionah.split_version(version2))
 
-@step(u'I have the Version object for (%(VERSION)s) and RegExp matcher for (%(VERSION)s)')
+
+@step(u'I have the Version object for (%(VERSION)s) and RegExp matcher for ' \
+      '(%(VERSION)s)')
 def have_version_object_and_re_obj(step, version1, version2):
     import re
     world.version1 = versionah.Version(versionah.split_version(version1))
