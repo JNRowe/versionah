5c42a854197fe5ceecb78efc55da174b1cf7a6f0
James Rowe <jnrowe@gmail.com>
2011-03-31 14:08:50 +0000

    Improve readability of step definitions.

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -4,12 +4,24 @@ import subprocess
 
 from nose.tools import assert_equal
 
-from lettuce import (step, world)
+from lettuce import step as lettuce_step
+from lettuce import world
 
 import versionah
 
 
-@step(u'I have the version (%s)' % versionah.VALID_VERSION)
+def step(match):
+    """Replace values in match strings with data from versionah module
+
+    The purpose is entirely to improve the look and readability of the steps
+    defined below, it provides nothing over hard coding the values in step
+    definitions.
+    """
+    valid_dict = dict([(s[6:], getattr(versionah, s)) for s in dir(versionah)
+                       if s.startswith("VALID_")])
+    return lettuce_step(match % valid_dict)
+
+@step(u'I have the version (%(VERSION)s)')
 def have_the_version(step, version):
     world.version = versionah.Version(versionah.split_version(version))
 
@@ -25,7 +37,7 @@ def bump_minor_version(step):
 def bump_micro_version(step):
     world.version.bump("micro")
 
-@step(u'I see the version (%s)' % versionah.VALID_VERSION)
+@step(u'I see the version (%(VERSION)s)')
 def see_the_version(step, expected):
     assert_equal(world.version.as_dotted(), expected)
 
@@ -61,8 +73,7 @@ def write_value_to_file(step, name):
     step.given("I read its content")
     os.unlink("tests/data/%s" % name)
 
-@step(u'I have the package (%s) version (%s)' % (versionah.VALID_PACKAGE,
-                                                 versionah.VALID_VERSION))
+@step(u'I have the package (%(PACKAGE)s) version (%(VERSION)s)')
 def have_named_version(step, name, version):
     world.version = versionah.Version(versionah.split_version(version), name)
 
@@ -78,8 +89,7 @@ def find_todays_date(step):
 def bump_patch_version(step):
     world.version.bump("patch")
 
-@step(u'I have the versions (%s) and (%s)' % (versionah.VALID_VERSION,
-                                              versionah.VALID_VERSION))
+@step(u'I have the versions (%(VERSION)s) and (%(VERSION)s)')
 def have_versions(step, version1, version2):
     world.version1 = versionah.Version(versionah.split_version(version1))
     world.version2 = versionah.Version(versionah.split_version(version2))
@@ -135,8 +145,7 @@ def process_with_linter(step, name, linter):
 def checker_returns_zero(step):
     assert_equal(world.retval, 0)
 
-@step(u'I have the version (%s) created on (%s)' % (versionah.VALID_VERSION,
-                                                    versionah.VALID_DATE))
+@step(u'I have the version (%(VERSION)s) created on (%(DATE)s)')
 def have_the_version_with_date(step, version, date):
     y, m, d = map(int, date.split("-"))
     world.version = versionah.Version(versionah.split_version(version),
@@ -150,20 +159,17 @@ def display_date_representation(step):
 def see_the_date(step, expected):
     assert_equal(world.string, expected)
 
-@step(u'I have the Version object for (%s) and (?:tuple|string) (%s)'
-       % (versionah.VALID_VERSION, versionah.VALID_VERSION))
+@step(u'I have the Version object for (%(VERSION)s) and (?:tuple|string) (%(VERSION)s)')
 def have_version_object_and_tuple(step, version1, version2):
     world.version1 = versionah.Version(versionah.split_version(version1))
     world.version2 = versionah.split_version(version2)
 
-@step(u'I have the Version object for (%s) and list (%s)'
-       % (versionah.VALID_VERSION, versionah.VALID_VERSION))
+@step(u'I have the Version object for (%(VERSION)s) and list (%(VERSION)s)')
 def have_version_object_and_list(step, version1, version2):
     world.version1 = versionah.Version(versionah.split_version(version1))
     world.version2 = list(versionah.split_version(version2))
 
-@step(u'I have the Version object for (%s) and RegExp matcher for (%s)'
-       % (versionah.VALID_VERSION, versionah.VALID_VERSION))
+@step(u'I have the Version object for (%(VERSION)s) and RegExp matcher for (%(VERSION)s)')
 def have_version_object_and_re_obj(step, version1, version2):
     import re
     world.version1 = versionah.Version(versionah.split_version(version1))
