051907e7cfdde1c3825f214add01c41cd2a145c6
James Rowe <jnrowe@gmail.com>
2011-09-18 17:22:14 +0100

    Link to source in Sphinx docs, where available.

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,8 @@ import cloud_sptheme as csp
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.%s" % ext for ext in ["autodoc", "intersphinx"]] + \
+extensions = ["sphinx.ext.%s" % ext
+              for ext in ["autodoc", "intersphinx", "viewcode"]] + \
     ["sphinxcontrib.%s" % ext for ext in ["blockdiag", "cheeseshop"]]
 
 # Add any paths that contain templates here, relative to this directory.
