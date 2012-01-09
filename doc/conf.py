4474cbec35b28e5012fe5ec3edd854d701440888
James Rowe <jnrowe@gmail.com>
2012-01-09 06:26:31 +0000

    Enable the coverage Sphinx extension.

--- doc/conf.py
+++ doc/conf.py
@@ -29,8 +29,9 @@ import cloud_sptheme as csp
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.%s" % ext
-              for ext in ["autodoc", "doctest", "intersphinx", "viewcode"]] + \
+extensions = \
+    ["sphinx.ext.%s" % ext for ext in ["autodoc", "coverage", "doctest",
+                                       "intersphinx", "viewcode"]] + \
     ["sphinxcontrib.%s" % ext for ext in ["blockdiag", "cheeseshop"]]
 
 # Add any paths that contain templates here, relative to this directory.
