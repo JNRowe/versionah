c4bd35a0c0c954a77f303f7543da38842a0444cd
James Rowe <jnrowe@gmail.com>
2012-07-24 14:49:45 +0100

    0.12.1 released.

--- versionah/_version.py
+++ versionah/_version.py
@@ -1,9 +1,9 @@
-# This is versionah version 0.12.0 (2012-07-19)
+# This is versionah version 0.12.1 (2012-07-24)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = "0.12.0"
-libtool = "12:20"
-hex = 0x000c00
-date = "2012-07-19"
-tuple = (0, 12, 0)
-web = "versionah/0.12.0"
+dotted = "0.12.1"
+libtool = "12:21"
+hex = 0x000c01
+date = "2012-07-24"
+tuple = (0, 12, 1)
+web = "versionah/0.12.1"
