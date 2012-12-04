3ac55d5f5b0a86f054a8720f7fa16ed844cff3f1
James Rowe <jnrowe@gmail.com>
2012-12-04 19:42:11 +0000

    Mark as Python 3.3 compatible.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Documentation',
         'Topic :: Software Development',
         'Topic :: Software Development :: Build Tools',
