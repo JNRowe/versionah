870e91723053b95f18e55d5f05dac53dbcb6be52
James Rowe <jnrowe@gmail.com>
2013-05-21 16:02:27 +0100

    0.13.1 released

--- versionah/_version.py
+++ versionah/_version.py
@@ -1,9 +1,9 @@
-# This is versionah version 0.13.0 (2013-05-09)
+# This is versionah version 0.13.1 (2013-05-21)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '0.13.0'
-libtool = '13:20'
-hex = 0x000d00
-date = '2013-05-09'
-tuple = (0, 13, 0)
-web = 'versionah/0.13.0'
+dotted = '0.13.1'
+libtool = '13:21'
+hex = 0x000d01
+date = '2013-05-21'
+tuple = (0, 13, 1)
+web = 'versionah/0.13.1'
