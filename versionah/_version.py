379e1e1956923dcc99cef31486376152a779f047
James Rowe <jnrowe@gmail.com>
2014-04-29 17:30:23 +0100

    Release 0.15.2

--- versionah/_version.py
+++ versionah/_version.py
@@ -1,10 +1,10 @@
-# This is versionah version 0.15.1 (2014-01-31)
+# This is versionah version 0.15.2 (2014-04-29)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '0.15.1'
-libtool = '15:21'
-hex = 0x000f01
-date = '2014-01-31'
-tuple = (0, 15, 1)
-dict = {'major': 0, 'micro': 1, 'minor': 15}
-web = 'versionah/0.15.1'
+dotted = '0.15.2'
+libtool = '15:22'
+hex = 0x000f02
+date = '2014-04-29'
+tuple = (0, 15, 2)
+dict = {'minor': 15, 'micro': 2, 'major': 0}
+web = 'versionah/0.15.2'
