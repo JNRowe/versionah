381bfce48783e3a3272c0096e042b17e01ec4fd7
James Rowe <jnrowe@gmail.com>
2015-01-30 00:15:08 +0000

    [Tr] Bump copyright years

--- extra/docrunner.py
+++ extra/docrunner.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # coding=utf-8
 """docrunner - Execute shell tests"""
-# Copyright © 2011-2014  James Rowe <jnrowe@gmail.com>
+# Copyright © 2011-2015  James Rowe <jnrowe@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
