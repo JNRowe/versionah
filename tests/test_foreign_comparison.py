96faec7666f29bbff3d8ac8cc7f75d3bbcbd2601
James Rowe <jnrowe@gmail.com>
2017-10-26 15:42:39 +0100

    [QA] Bump copyright years
    This uses Greg's full amazing new updater, and you can colour me
    impressed.

--- tests/test_foreign_comparison.py
+++ tests/test_foreign_comparison.py
@@ -1,21 +1,21 @@
 #
 # coding=utf-8
 """test_foreign_comparison - Foreign comparison tests"""
-# Copyright © 2011-2015  James Rowe <jnrowe@gmail.com>
+# Copyright © 2012-2017  James Rowe <jnrowe@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of versionah.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# versionah is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# versionah is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License along with
+# versionah.  If not, see <http://www.gnu.org/licenses/>.
 
 from pytest import mark, raises
 
--- versionah/vcs.py
+++ versionah/vcs.py
@@ -1,21 +1,21 @@
 #
 # coding=utf-8
 """vcs - Version interface for versionah"""
-# Copyright © 2011-2015  James Rowe <jnrowe@gmail.com>
+# Copyright © 2014-2015  James Rowe <jnrowe@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of versionah.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# versionah is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# versionah is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License along with
+# versionah.  If not, see <http://www.gnu.org/licenses/>.
 
 import errno
 
