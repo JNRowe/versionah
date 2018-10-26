6eea5e0db0e6ce069a88896616bed47406d67db4
James Rowe <jnrowe@gmail.com>
2018-10-26 06:57:13 +0100

    [Tr] Sync pocoo intersphinx URLs

--- doc/conf.py
+++ doc/conf.py
@@ -126,8 +126,8 @@ extlinks = {
 intersphinx_mapping = {
     k: (v, os.getenv('SPHINX_{}_OBJECTS'.format(k.upper())))
     for k, v in {
-        'click': 'http://click.pocoo.org/6/',
-        'jinja2': 'http://jinja.pocoo.org/docs/',
+        'click': 'https://click.palletsprojects.com/en/7.x/',
+        'jinja2': 'http://jinja.pocoo.org/docs/2.10/',
         'jnrbase': 'https://jnrbase.readthedocs.io/en/latest/',
         'python': 'https://docs.python.org/3/',
     }.items()
