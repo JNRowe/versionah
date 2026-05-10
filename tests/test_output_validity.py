0cd61b45200fe8a70fa319cf898ac2519be13457
James Rowe <jnrowe@gmail.com>
2026-05-10 11:56:20 +0100

    ci: Update Python interpreters for recent envs

--- tests/test_output_validity.py
+++ tests/test_output_validity.py
@@ -33,7 +33,7 @@ from versionah.cmdline import CliVersion, guess_type
     [
         ("1.0.1", "test_wr.c", "splint"),
         ("1.0.1", "test_wr.m4", "m4 -P -E -E"),
-        ("1.0.1", "test_wr.py", "python -W all"),
+        ("1.0.1", "test_wr.py", "python3 -W all"),
         ("1.0.1", "test_wr.rb", "ruby -c"),
     ],
 )
